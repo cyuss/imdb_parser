81360819a828f3c5876f08b04747c52b44ae1381
Cyuss <maamra.youcef@gmail.com>
2017-03-08 16:07:01 +0100

    correct code indentation

--- imdb_crawler.py
+++ imdb_crawler.py
@@ -5,19 +5,20 @@ import json
 """
 	find a movie using imdb api with the title and the year
 """
-def find_movie(title, year, format = "json") :
+def find_movie(title, year, format="json"):
 	# http://www.omdbapi.com/?t=legend&y=2015&plot=short&r=json
 	t = title.replace(" ", "+")
 	url = "http://www.omdbapi.com/?t="+t+"&y="+str(year)+"&plot=full&r="+format
 
 	request = urllib2.Request(url)
 	response = json.load(urllib2.urlopen(request))
-	data = json.dumps(response, indent = 2)
+	data = json.dumps(response, indent=2)
 	print data
 	return data
 
 
-title = raw_input("Please enter a movie title: ")
-year = raw_input("Which year? ")
-description = find_movie(title, year)
-#find_movie("close range", 2015)
+if __name__ == "__main__":
+	title = raw_input("Please enter a movie title: ")
+	year = raw_input("Which year? ")
+	description = find_movie(title, year)
+	#find_movie("close range", 2015)
